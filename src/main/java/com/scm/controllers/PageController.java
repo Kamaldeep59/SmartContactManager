85e179bbc6f7dfd524ef8dff7dcf2bda5e98b335
Kamaldeep59 <105438212+Kamaldeep59@users.noreply.github.com>
2025-03-26 17:47:05 +0530

    yes

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,14 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.ui.Model;
 
 @Controller
 public class PageController {
 
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String home(Model model) {
         System.out.println("PageController: Passing through...");
         model.addAttribute("name", "kamalsokhal");
@@ -16,5 +17,37 @@ public class PageController {
         model.addAttribute("date", new java.util.Date());
         return "home";
     }
+    
+    @GetMapping("/about")
+    public String about(Model model) {
+        System.out.println("PageController: Passing through...");
+        model.addAttribute("title", "About");
+        return "about";
+    }
+    @GetMapping("/services")
+    public String services(Model model) {
+        System.out.println("PageController: Passing through...");
+        model.addAttribute("title", "Services");
+        return "services";
+    }
+    @GetMapping("/contact") 
+    public String contact(Model model) {
+        System.out.println("PageController: Passing through...");
+        model.addAttribute("title", "Contact");
+        return "contact";
+    }
+    @GetMapping("/register")  
+    public String register(Model model) {
+        System.out.println("PageController: Passing through...");
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+    @GetMapping("/login") 
+    public String login(Model model) {
+        System.out.println("PageController: Passing through...");
+        model.addAttribute("title", "Login");
+        return "login";
+    }    
+
 
 }
